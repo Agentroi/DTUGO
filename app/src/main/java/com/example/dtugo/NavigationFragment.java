3eaeda255a09ed8c9f566e4660ed44350653ce60
Lomo0841 <lomo0841@gmail.com>
2021-06-14 15:28:36 +0200

    Initial layout for the map activity

---
+++ app/src/main/java/com/example/dtugo/NavigationFragment.java
@@ -0,0 +1,45 @@
+package com.example.dtugo;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class NavigationFragment extends Fragment {
+    private View view;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        view = inflater.inflate(R.layout.fragment_navigation, container, false);
+        Button mapButton = (Button) view.findViewById(R.id.toMapButton);
+        mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                MapFragment mapFragment = new MapFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.fragment_container, mapFragment).commit();
+
+            }
+        });
+
+        Button listButton = (Button) view.findViewById(R.id.toListButton);
+        listButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ListFragment listFragment = new ListFragment();
+                FragmentManager fragmentManager = getFragmentManager();
+                fragmentManager.beginTransaction().replace(R.id.fragment_container, listFragment).commit();
+
+            }
+        });
+
+
+
+
+        return view;
+    }
+}
