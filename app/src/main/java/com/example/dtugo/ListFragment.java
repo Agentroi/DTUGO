a86776e626f4b5dcfd2146678d4fa4cdd722889d
Lomo0841 <lomo0841@gmail.com>
2021-06-15 13:00:14 +0200

    Implemented ListView functionality

--- app/src/main/java/com/example/dtugo/ListFragment.java
+++ app/src/main/java/com/example/dtugo/ListFragment.java
@@ -1,14 +1,52 @@
 package com.example.dtugo;
 
+import android.app.ListActivity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ListFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_list, container, false);
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        String[] POI = {"Option1", "Option2", "Option3"};
+
+        ListView listView = view.findViewById(R.id.list);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String>(
+                getActivity(),
+                android.R.layout.simple_list_item_1,
+                POI
+        );
+
+        listView.setAdapter(listViewAdapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                // Display a Toast message indicting the selected item
+                Toast.makeText(getActivity().getApplicationContext(),
+                        position + "", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        return view;
     }
+
+
+
+
 }
